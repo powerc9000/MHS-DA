7ef6a9cc49d5dcba2d46af8f16886b573eaa63d3
powerc9000 <clay_murray@cmwebadventures.com>
2011-10-10 20:14:16 -0600

    hi

--- db/migrate/20111006031859_addname.rb
+++ db/migrate/20111006031859_addname.rb
@@ -4,15 +4,15 @@ class Addname < ActiveRecord::Migration
     change_table :users do |t|
       
       t.text  :fname
-     
+      
       t.text  :lname
       t.text  :house
-        
+      t.text :email, :unique => true, :case_sensitive =>false
       t.integer  :rank
       t.text :photo
       
     end
-   change_column :users, :email, :string, :unique => true, :case_sensitive =>false
+   
    change column :users, :username, :string, :unique => true, :case_sensitive =>false
   end
 
